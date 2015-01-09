fd50930b297c7070b7433c0abc7a3a7f9655f604
mapdv.ru@gmail.com <mapdv.ru@gmail.com@71dbe1cd-ede2-9f14-98df-fe2607c1c770>
2015-01-09 10:23:47 +0000

    Начальная заливка карт и файлов. Версия полностью соответствует ревизии 2152 репозитория atlas-dv3.

---
+++ cmd/O-53.cmd
@@ -0,0 +1,16 @@
+set N=O-53
+
+cd ..
+mkdir %temp%\mapconv
+copy %N%.mp %temp%\mapconv\map.mp
+copy soft\*.exe %temp%\mapconv
+copy soft\map.cfg %temp%\mapconv\map.cfg
+copy soft\%N%.txt %temp%\mapconv\map.txt
+
+%systemdrive%
+cd %temp%\mapconv
+mp2mp.exe -h "map.txt" "map.cfg" "map-m.mp"
+rename map-m.mp %N%-%date%.mp
+%N%-%date%.mp
+del %temp%\mapconv /S /Q /F
+rmdir %temp%\mapconv
